0cc8fb1bd497fecf2385407c0f3f6357c178d06d
totoshkaa <abdunazarov20090228@gmail.com>
2024-06-11 09:59:52 +0500

    first commit

--- goodreads/urls.py
+++ goodreads/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', include('books.urls'))
+    path('books/', include('books.urls')),
+    path('',index)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
